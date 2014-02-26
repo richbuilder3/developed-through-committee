98429d56575de560612821a1bbe2a7446d26a5fc
Richbuilder3 <richard.c.grundy@gmail.com>
2014-02-26 18:44:42 -0500

    added devise and 5 devise steps

--- dtc/config/routes.rb
+++ dtc/config/routes.rb
@@ -1,4 +1,10 @@
 Dtc::Application.routes.draw do
+
+
+  devise_for :admins
+ root :to => "home#index"
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
